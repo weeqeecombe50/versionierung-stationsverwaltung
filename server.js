7146c2760991b749b1b63c39c72100f030c6cf4e
weeqeecombe50 <mynieagbona@hotmail.com>
2025-09-03 20:13:48 +0300

    Update MongoDB connection URL to use process.env.DB_URL for flexibility in environment configuration.

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const DB_NAME = process.env.DB_NAME || 'versionierung-stationsverwaltung';
+const DB_URL = process.env.DB_URL || 'mongodb://localhost/versionierung-stationsverwaltung';
 
 app.use(cors());
 app.use(express.json());
 
 // MongoDB Verbindung
-mongoose.connect(`mongodb://localhost/${DB_NAME}`, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB verbunden'))
   .catch(err => console.log(err));
 
