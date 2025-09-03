04501849e7d8db96a8a2eb1b5944a5e5cded4782
weeqeecombe50 <mynieagbona@hotmail.com>
2025-09-03 20:12:00 +0300

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Versionierung Stationsverwaltung</h1>
+      <p>Willkommen zur Stationsverwaltung!</p>
+    </div>
+  );
+}
+
+export default App;
