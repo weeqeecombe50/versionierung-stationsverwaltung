74a15aba41a7058f6011a4d404803fc5e849eabd
weeqeecombe50 <mynieagbona@hotmail.com>
2025-09-03 20:12:48 +0300

    Fix MongoDB connection string to use environment variable for database name

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const DB_NAME = process.env.DB_NAME || 'versionierung-stationsverwaltung';
 
 app.use(cors());
 app.use(express.json());
 
 // MongoDB Verbindung
-mongoose.connect('mongodb://localhost/versionierung-stationsverwaltung', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(`mongodb://localhost/${DB_NAME}`, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB verbunden'))
   .catch(err => console.log(err));
 
